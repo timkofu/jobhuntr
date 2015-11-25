e57fab637a509105805533f564ec12d568b2bb5e
Timothy Makobu <makobu.mwambiriro@gmail.com>
2015-11-25 14:42:16 +0300

    data modeling initial

--- search/models.py
+++ search/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from spider.models import SourceLinks
+
+
+class JobsData(models.Model):
+
+    source = models.ForeignKey(SourceLinks)
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    collected_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
