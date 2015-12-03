af1a8ff079c898a871f0cfc5c24f92973c9a4fa4
Timothy Makobu <makobu.mwambiriro@gmail.com>
2015-12-03 16:44:09 +0300

    remove_expired command bug-fix

--- search/management/commands/remove_expired.py
+++ search/management/commands/remove_expired.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         try:
-            JobsData.objects.filter(add_time__lte=datetime.now()-timedelta(days=28))
+            JobsData.objects.filter(added_on__lte=datetime.now()-timedelta(days=28))
         except Exception as e:
             raise CommandError(e.message)
 
