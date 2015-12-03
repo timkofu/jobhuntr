ecda461f1020a672d9b3835d63a9a273f4bcd548
Timothy Makobu <makobu.mwambiriro@gmail.com>
2015-12-03 16:48:44 +0300

    cron.sh improvemnts

--- cron.sh
+++ cron.sh
@@ -3,7 +3,7 @@
 cd /opt/code/jobhuntr/spidr
 /opt/anaconda2/bin/scrapy crawl jobhuntr
 
-/bin/sleep 1
-
 /opt/anaconda2/bin/python /opt/code/jobhuntr/manage.py remove_expired
-/usr/bin/yes | /opt/anaconda2/bin/python /opt/code/jobhuntr/manage.py rebuild_index
+
+# email error only if stderr is written to
+/usr/bin/yes | /opt/anaconda2/bin/python /opt/code/jobhuntr/manage.py rebuild_index > /dev/null
