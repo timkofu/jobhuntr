72bb68e213c5b58a0cd3913884717f128b7475b0
Timothy Makobu <makobu.mwambiriro@gmail.com>
2015-12-07 11:08:22 +0300

    behinning tests

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test import Client
+from django.test.utils import setup_test_environment
+setup_test_environment()
 
-# Create your tests here.
+
+class Blankettests(TestCase):
+
+    def test_main_page_loads(self):
+        self.assertEqual(Client().get('/').status_code, 200)
+
+    def test_countries_loads(self):
+        self.assertEqual(Client().get('/countries/').status_code, 200)
+
+    def test_search_works(self):
+        response = Client().get('/?q=KE')
+        self.assertEqual(response.status_code, 200)
