130881245d8d82805ec057892ae2671dc75f4eba
Timothy Makobu <makobu.mwambiriro@gmail.com>
2015-12-07 11:38:18 +0300

    behinning tests

--- search/tests.py
+++ search/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test import Client
+from django.core.urlresolvers import reverse
 from django.test.utils import setup_test_environment
 setup_test_environment()
 
@@ -10,7 +11,7 @@ class Blankettests(TestCase):
         self.assertEqual(Client().get('/').status_code, 200)
 
     def test_countries_loads(self):
-        self.assertEqual(Client().get('/countries/').status_code, 200)
+        self.assertEqual(Client().get(reverse('countries')).status_code, 200)
 
     def test_search_works(self):
         response = Client().get('/?q=KE')
