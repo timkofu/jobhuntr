3bb0b96a95569820aab93cf79631f1690612c68d
Tim <makobu.mwambiriro@gmail.com>
2015-12-12 10:17:52 +0300

    expanded test coverage

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,7 @@
+from webtest import TestApp
+
+from jobhuntr.wsgi import application
+
 from django.test import TestCase
 from django.test import Client
 from django.utils.six import StringIO
@@ -16,10 +20,16 @@ class Blankettests(TestCase):
         self.assertEqual(Client().get(reverse('countries')).status_code, 200)
 
     def test_search_works(self):
-        response = Client().get('/?q=KE')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Client().get('/?q=KE').status_code, 200)
 
     def test_remove_expired_management_command(self):
         out = StringIO()
         call_command('remove_expired', stdout=out)
         self.assertIn("", out.getvalue())
+
+    def test_admin_loads(self):
+        self.assertEqual(Client().get('/admin/', follow=True).status_code, 200)
+
+    def test_wsgi_loads(self):
+        app = TestApp(application)
+        self.assertEqual(app.get('/').status_int, 200)
