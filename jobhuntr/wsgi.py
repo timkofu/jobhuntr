cad094e9de747fd5927e56e320ad866367be740a
Tim <makobu.mwambiriro@gmail.com>
2016-06-05 17:12:00 +0300

    settings fixings

--- jobhuntr/wsgi.py
+++ jobhuntr/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobhuntr.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobhuntr.settings")
 
 application = DjangoWhiteNoise(get_wsgi_application())
