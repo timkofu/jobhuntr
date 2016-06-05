76d5145de0e38974e269aa8591e04f4479dc1c42
Tim <makobu.mwambiriro@gmail.com>
2016-06-05 18:17:22 +0300

    autoquery will need its own search model

--- jobhuntr/urls.py
+++ jobhuntr/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     # #url(r'.*', 'search.views.search', name='search'),
     url(r'^countries/$', countries, name='countries'),
     url(r'.*', SearchView(
-            searchqueryset=SearchQuerySet().auto_query().order_by('-added_on')
+            #searchqueryset=SearchQuerySet().auto_query().order_by('-added_on')
+            searchqueryset=SearchQuerySet().order_by('-added_on')
         )
     ),
 ]
